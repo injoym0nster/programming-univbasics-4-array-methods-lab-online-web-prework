92ad3d0daf10d7ae0da980d84561ea6a9c9e6b42
injoym0nster <53123532+injoym0nster@users.noreply.github.com>
2019-08-09 14:21:36 -0500

    done.rb

---
+++ lib/done.rb
@@ -0,0 +1,23 @@
+def using_include(4 , 16)
+
+end
+
+def using_sort(2,10)
+
+end
+
+def using_reverse(10,9)
+
+end
+
+def using_first(800,60)
+
+end
+
+def using_last(2,4)
+
+end
+
+def using_size(1,99)
+
+end
